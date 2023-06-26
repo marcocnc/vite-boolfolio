3cc43ca9915b2ede8003044549064be672e20b5d
marcocnc <marcocancelliere56@gmail.com>
2023-06-26 18:49:01 +0200

    created store with apiUrl and axios call

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import {store} from './data/store';
+import axios from 'axios';
+export default {
+  name: 'Home',
+  components:{
+        store
+      },
+
+      methods:{
+        getApi(){
+          axios.get(store.apiUrl + 'posts')
+          .then(results => {
+            console.log(results);
+          })
+        }
+      },
+
+      mounted(){
+        this.getApi()
+      }
+}
 </script>
 
+
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="container bg-dark">
+    <h1 class="text-white">test</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
+<style lang="scss">
+
 </style>
